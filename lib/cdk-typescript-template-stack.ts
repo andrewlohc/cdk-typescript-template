cdef1b45ec19dec2692ab9b044cf86aff9e8c572
Andrew Lo <andrewlohc@gmail.com>
2024-07-18 01:52:29 -0700

    feat: initial commit

---
+++ lib/cdk-typescript-template-stack.ts
@@ -0,0 +1,10 @@
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+
+export class CdkTypeScriptTemplateStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+  }
+}
