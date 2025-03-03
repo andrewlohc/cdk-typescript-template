b0143a5deb91fb41847e4a664217f91d993974f9
Andrew Lo <andrewlohc@outlook.com>
2025-03-02 16:31:45 -0800

    chore: update cdk libraries and tsx

--- lib/cdk-typescript-template-stack.ts
+++ lib/cdk-typescript-template-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
+// const __dirname = import.meta.dirname;  
+
 export class CdkTypeScriptTemplateStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
